37ac6ba423b24c5bae5af78972ee97f3b32d941b
anjvik020 <anjalijaiswal402@gmail.com>
2026-02-10 17:17:55 +0530

    added a anjali class in src test java

---
+++ NinzaCrmadvance/src/test/java/CreateCampaign/Anjali.java
@@ -0,0 +1,12 @@
+package CreateCampaign;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Anjali {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hiiii");
+	}
+
+}
